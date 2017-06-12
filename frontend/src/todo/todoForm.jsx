654180c6de12458e9e4711f271419d14b86a1b43
Tayane Fernandes <tayane.rmf@gmail.com>
2017-06-12 17:07:23 -0300

    Add form and list components

---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default props => (
+  <div role='form' className='todoForm'>
+    <div className='col-xs-12 col-sm-9 col-md-10'>
+      <input id='description' className='form-control' placeholder='Adicione uma tarefa' />
+    </div>
+    <div className='col-xs-12 col-sm-9 col-md-10'>
+      <button className='btn btn-primary'>
+        <i className='fa fa-plus'></i>
+      </button>
+    </div>
+  </div>
+)
