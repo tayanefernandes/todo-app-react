7b613b3361b5404e2d8104d446681177c5f83d2e
Tayane Fernandes <tayane.rmf@gmail.com>
2017-06-13 15:14:42 -0300

    Create the funcionality to add and remove items

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
   constructor(props) {
@@ -9,6 +13,13 @@ export default class Todo extends Component {
     this.state = { description: '', list: []}
     this.handleAdd = this.handleAdd.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
+    this.refresh()
+  }
+
+  refresh() {
+    axios.get(`${URL}?sort=-createdAt`)
+    .then(resp => this.setState({...this.state, description: '', list: resp.data}))
   }
 
   handleChange(e) {
@@ -16,7 +27,14 @@ export default class Todo extends Component {
   }
 
   handleAdd() {
-    console.log(this)
+    const description = this.state.description
+    axios.post(URL, { description })
+    .then(resp => this.refresh())
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${URL}/${todo._id}`)
+      .then(resp => this.refresh())
   }
 
   render() {
@@ -25,7 +43,8 @@ export default class Todo extends Component {
         <PageHeader name='Tarefas' small='Cadastro' />
         <TodoForm description={this.state.description}
           handleChange={this.handleChange} handleAdd={this.handleAdd} />
-        <TodoList />
+        <TodoList list={this.state.list}
+          handleRemove={this.handleRemove}/>
       </div>
     )
   }
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -9,8 +9,7 @@ export default props => (
          value={props.description} onChange={props.handleChange}/>
     </Grid>
     <Grid cols='12 3 2'>
-      <IconButton style='primary' icon='plus' onClick={props.handleAdd}
-          onclick={props.handleAdd}></IconButton>
+      <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
     </Grid>
   </div>
 )
