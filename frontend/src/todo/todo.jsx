c9f2586f1538a8e32ecd2683fc000b518e8aea2f
Tayane Fernandes <tayane.rmf@gmail.com>
2017-06-13 15:36:27 -0300

    Add 'done and undo' buttons

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,6 +14,8 @@ export default class Todo extends Component {
     this.handleAdd = this.handleAdd.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.handleRemove = this.handleRemove.bind(this)
+    this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+    this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
     this.refresh()
   }
 
@@ -37,13 +39,26 @@ export default class Todo extends Component {
       .then(resp => this.refresh())
   }
 
+  handleMarkAsDone(todo) {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+      .then(resp => this.refresh())
+  }
+
+  handleMarkAsPending(todo) {
+    axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+      .then(resp => this.refresh())
+  }
+
   render() {
     return (
       <div>
         <PageHeader name='Tarefas' small='Cadastro' />
         <TodoForm description={this.state.description}
-          handleChange={this.handleChange} handleAdd={this.handleAdd} />
+          handleChange={this.handleChange}
+          handleAdd={this.handleAdd} />
         <TodoList list={this.state.list}
+          handleMarkAsDone={this.handleMarkAsDone}
+          handleMarkAsPending={this.handleMarkAsPending}
           handleRemove={this.handleRemove}/>
       </div>
     )
