497a73a4f581572ca3c22d6cf61c684ac34b65f8
Tayane Fernandes <tayane.rmf@gmail.com>
2017-06-19 15:10:10 -0300

    Add reducers and actions for the todo list

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,22 +1,8 @@
 import { combineReducers } from 'redux'
+import todoReducer from '../todo/todoReducer'
 
 const rootReducer = combineReducers({
-  todo: () => ({
-    description: 'Opaaaaa',
-    list: [{
-      _id: 1,
-      description: 'Pagar fatura do cartao',
-      done: true
-    }, {
-      _id: 2,
-      description: 'Reuniao com equipe',
-      done: false
-    }, {
-      _id: 3,
-      description: 'Consulta na terça',
-      done: false
-    }]
-  })
+  todo: todoReducer
 })
 
 export default rootReducer
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,33 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeDescription, search } from './todoActions'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-const TodoForm = props => {
-  const keyHandler = (e) => {
+class TodoForm extends Component {
+  constructor(props) {
+    super(props)
+    this.keyHandler = this.keyHandler.bind(this)
+  }
+
+  componentWillMount() {
+    this.props.search()
+  }
+
+  keyHandler(e) {
     if(e.key === 'Enter') {
-      e.shiftKey ? props.handleSearch() : props.handleAdd()
+      e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
     } else if (e.key === 'Escape') {
-      props.handleClear()
+      this.props.handleClear()
     }
   }
-  return (
-    <div role='form' className='todoForm'>
-      <Grid cols='12 9 10'>
-        <input id='description' className='form-control' placeholder='Adicione uma tarefa'
-           value={props.description}
-           onChange={props.handleChange}
-           onKeyUp={keyHandler}/>
-      </Grid>
-      <Grid cols='12 3 2'>
-        <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
-        <IconButton style='info' icon='search' onClick={props.handleSearch}></IconButton>
-        <IconButton style='default' icon='close' onClick={props.handleClear}></IconButton>
-      </Grid>
-    </div>
-  )
+
+  render() {
+    return (
+      <div role='form' className='todoForm'>
+        <Grid cols='12 9 10'>
+          <input id='description' className='form-control' placeholder='Adicione uma tarefa'
+             value={this.props.description}
+             onChange={this.props.changeDescription}
+             onKeyUp={this.keyHandler}/>
+        </Grid>
+        <Grid cols='12 3 2'>
+          <IconButton style='primary' icon='plus' onClick={this.props.handleAdd}></IconButton>
+          <IconButton style='info' icon='search' onClick={this.props.handleSearch}></IconButton>
+          <IconButton style='default' icon='close' onClick={this.props.handleClear}></IconButton>
+        </Grid>
+      </div>
+    )
+  }
 }
+
 
 const mapStateToProps = state => (
   {
@@ -35,4 +50,8 @@ const mapStateToProps = state => (
   }
 )
 
-export default connect(mapStateToProps)(TodoForm)
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({ changeDescription, search }, dispatch)
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
